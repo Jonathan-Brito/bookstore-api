8b2a599c018b790f37b806577cd482b02c991e94
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-01-23 19:25:41 -0300

    Dev profile created

---
+++ src/main/java/com/jonathan/brito/bookstore/config/Devconfig.java
@@ -0,0 +1,29 @@
+package com.jonathan.brito.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jonathan.brito.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class Devconfig {
+	
+	@Autowired
+	private DBService debService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciaBaseDados() {
+		if(strategy.equals("create")) {
+			this.debService.instanciaBaseDados();
+		}
+		return false;
+	}
+
+}
