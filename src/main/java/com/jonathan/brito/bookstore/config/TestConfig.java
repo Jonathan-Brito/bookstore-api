e60ad855bf5c1f0176132a8603951be636c5389d
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-01-23 17:45:36 -0300

    added implemented test profile

---
+++ src/main/java/com/jonathan/brito/bookstore/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.jonathan.brito.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jonathan.brito.bookstore.service.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbservice;
+	
+	@Bean
+	public void instanciaBaseDados() {
+		this.dbservice.instanciaBaseDados();
+	}
+
+}
---
+++ src/main/java/com/jonathan/brito/bookstore/service/DBService.java
@@ -0,0 +1,32 @@
+package com.jonathan.brito.bookstore.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jonathan.brito.bookstore.domain.Categoria;
+import com.jonathan.brito.bookstore.domain.Livro;
+import com.jonathan.brito.bookstore.repositories.CategoriaRepository;
+import com.jonathan.brito.bookstore.repositories.LivroRepository;
+
+public class DBService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
+
+	public void instanciaBaseDados() {
+
+		Categoria cat1 = new Categoria(null, "Informática", "livros de TI");
+
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem", cat1);
+
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
+
+}
